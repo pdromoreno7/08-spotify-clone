2442e29d7bce5931e3a66f6cdffd27e7aeb40f01
pdromoreno7 <pdromorenodev@gmail.com>
2024-01-31 07:04:19 -0500

    feat: header component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,19 @@
+import { Header } from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-green-500">Hello World de nuevo</h1>
-    </main>
+    <div
+      className="
+    bg-neutral-900 
+    rounded-lg 
+    h-full 
+    w-full 
+    overflow-hidden 
+    overflow-y-auto
+  "
+    >
+      <Header>header</Header>
+    </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {};
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b  from-emerald-800 p-6`,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
